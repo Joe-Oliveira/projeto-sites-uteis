1dc294195b5fbf9e27941f8a50df35e2a74294be
Joe <124465168+Joe-Oliveira@users.noreply.github.com>
2025-01-26 11:24:30 -0300

    algumas atualizações e JQuery

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    ScrollReveal().reveal('#home', {
+        origin: 'left',
+        duration: 1000,
+        distance: '30%'
+    });
+
+    ScrollReveal().reveal('.sites', {
+        origin: 'left',
+        duration: 2000,
+        distance: '30%'
+    });
+});
